d42a6737b4ccd2c285925bd595f5f0e5bd99af06
7920000 <palestinedrink.italia@hotmail.com>
2025-04-22 21:38:56 +0200

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Palestine Drinks 🥤</h1>
+      <p>Il sito è attivo e connesso correttamente! ✨</p>
+      <p>Ora puoi personalizzarlo come vuoi.</p>
+    </div>
+  );
+}
